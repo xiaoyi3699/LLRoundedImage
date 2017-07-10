37fb5d8bdb72f9e28a534d58fbef3ea3c5cbdb89
王 <122589615@qq.com>
2017-07-10 16:16:53 +0800

    Initial Commit

---
+++ LLRoundedImage/LLViewController.h
@@ -0,0 +1,15 @@
+//
+//  LLViewController.h
+//  LLRoundedImage
+//
+//  Created by WangZhaomeng on 2017/7/10.
+//  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLViewController : UIViewController
+
+
+@end
+
---
+++ LLRoundedImage/LLViewController.m
@@ -0,0 +1,29 @@
+//
+//  LLViewController.m
+//  LLRoundedImage
+//
+//  Created by WangZhaomeng on 2017/7/10.
+//  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import "LLViewController.h"
+
+@interface LLViewController ()
+
+@end
+
+@implementation LLViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
